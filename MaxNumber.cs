7db5a9481dc6e061721c6c22ef920e3027d5479b
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-28 18:17:31 +0530

    finding the maximum Integers

---
+++ MaxNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class MaxNumber
+    {
+        
+        internal static int FindMaxNumber(int firstValue, int secondValue, int ThirdValue)
+        {
+            if (firstValue.CompareTo(secondValue)> 0 && firstValue.CompareTo(ThirdValue)>0)
+            {
+                return firstValue;
+            }
+            if(secondValue.CompareTo(firstValue)>0 && secondValue.CompareTo(ThirdValue)>0)
+            {
+                return secondValue;
+            }
+            if(ThirdValue.CompareTo(firstValue)>0 && ThirdValue.CompareTo(secondValue)>0)
+            {
+                return ThirdValue;
+            }
+            throw new Exception("all tha values are same");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxNumber.FindMaxNumber(30,20,10));
+        }
+    }
+}
