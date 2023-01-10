089c2d1cb131bbd169e74e619aaef67e140ecdb9
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2023-01-10 16:08:35 +0530

    Refactor all the 3 to One Generic Method and find the maximum

---
+++ MaxNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class MaxNumber
+    {
+        internal static T FindMaxNumber<T>(T FirstValue, T secondValue, T ThirdValue) where T : IComparable
+        {
+
+            if (FirstValue.CompareTo(secondValue) > 0 && FirstValue.CompareTo(ThirdValue) > 0)
+            {
+                return FirstValue;
+            }
+            if(secondValue.CompareTo(FirstValue)>0 && secondValue.CompareTo(ThirdValue)>0)
+            {
+                return secondValue;
+            }
+            if(ThirdValue.CompareTo(FirstValue)>0 && ThirdValue.CompareTo(secondValue)>0)
+            {
+                return ThirdValue;
+            }
+            throw new Exception("all tha values are same");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxNumber.FindMaxNumber<int>(12,19,14));
+        }
+    }
+}
